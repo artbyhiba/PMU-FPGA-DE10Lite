36356593bae1c05a431b2c994f6d80aa097d0903
Hiba Naeem <hiba.naeem@student.kit.edu>
2026-02-03 13:02:54 +0100

    Update main.c

--- nios_software/main.c
+++ nios_software/main.c
@@ -1,6 +1,24 @@
-main.c
-adc_read.c
-adc_read.h
-pwm_control.c
-pwm_control.h
-system.h   (auto from Quartus / BSP)
+#include "system.h"
+#include "io.h"
+#include <stdio.h>
+#include "adc_read.h"
+#include "pwm_control.h"
+
+int main()
+{
+    printf("PMU FPGA System Started\n");
+
+    while(1)
+    {
+        int adc_value = adc_read();
+
+        // Scale ADC to PWM range
+        int pwm_value = adc_value >> 2;   // 12 bit → 10 bit
+
+        pwm_set_duty(pwm_value);
+
+        printf("ADC: %d  PWM: %d\n", adc_value, pwm_value);
+    }
+
+    return 0;
+}
